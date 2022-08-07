f070303fd2dbd8ee85a3b6f98e2ede883b60a9b6
iamzzg <785864219@qq.com>
2022-08-08 00:00:42 +0800

    feat: 增加github-pages部署脚本

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# 发生错误时终止
+set -e
+
+# 构建
+npm run build
+
+# 进入构建文件夹
+cd dist
+
+# 如果你要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+git branch -M gh-pages
+
+
+# 如果你要部署在 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
+git remote add origin git@github.com:iamzzg/vue3-js-element-plus-template.git
+git push -f origin gh-pages
+
+cd -
